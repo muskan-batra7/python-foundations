55dcf9fefbf9f6501ead71b2e737634101094b2a
muskan <muskan20071676@gmail.com>
2026-06-10 16:28:26 +0530

    Day 2 - variables and data types

---
+++ Day02_variables.py
@@ -0,0 +1,14 @@
+first_name="Muskan"
+last_name="Batra"
+age=19
+tgpa=9.32
+is_pass=True
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(tgpa))
+print(type(is_pass))
+
+print(int(tgpa))
+print(str(age))
+print(float(age))
