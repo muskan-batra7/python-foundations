057cd30c3ee0707bceb957f4d75381e92d3f4ab5
muskan <muskan20071676@gmail.com>
2026-06-20 23:34:50 +0530

    Day 4 - numbers, operators, grade calculator

---
+++ Day04_numbers.py
@@ -0,0 +1,42 @@
+a=5
+b=7
+c=9
+#arithmetic
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+#assignment (+=,-=,*=,%=,//=,**=,&=,^=,>>=,<<=,:=)
+#ternary
+d="fri" if a==5 else "sat" if a==6 else "sun"
+print(d)
+#comparison(==,!=,>,<,>=,<=)
+print(a==b)
+print(a!=b)
+#llogical(and ,or ,not)
+print(a>5 and b>6)
+print(a>5 or b>8)
+print (not(a>6 and b>5))
+#identity(is ,is not)
+print(a is b)
+print(c is not b)
+#membership(in ,not in)
+#bitwise(&,!,^(XOR),~(Not),<<(left shift),>>(right shift))
+#grade calculator
+sub1=88
+sub2=89
+sub3=79
+sub4=91
+total=sub1+sub2+sub3+sub4
+average=total/4
+percentage= (total/400)*100
+print(f"total marks : {total}")
+print(f"average marks : {average}")
+print(f"percentage :{percentage}")
+
+
+
+
