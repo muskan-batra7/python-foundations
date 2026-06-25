6f3f1ba086ef93e62efaf4bf5e424162c257e2a4
muskan <muskan20071676@gmail.com>
2026-06-25 22:58:36 +0530

    Day 8 - while loops, number guessing game

--- Day08_while.py
+++ Day08_while.py
@@ -12,4 +12,21 @@ while True:
         print("High! guess lower")
     else:
         print(f"Correct! you guessed in {attempts} attempts")
+        break 
+
+#countdown
+count =10
+while count>=0:
+    print(f"countdown:{count}")
+    count-=1
+print("Blast off! 🚀")
+
+#sum until user enters 0
+total=0
+while True:
+    num=int(input("Enter number you want to add: "))
+    if num==0:
+        print("You entered 0")
         break
+    total+=num
+print(f"Total: {total}")
