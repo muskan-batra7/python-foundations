189bf4de6da4558f3fadcfe9f149b2add6a50cd5
muskan <muskan20071676@gmail.com>
2026-06-26 23:43:49 +0530

    Day 9 - Functions

--- Day09_functions.py
+++ Day09_functions.py
@@ -11,6 +11,24 @@ def greet_student(name,college="LPU"):
 def stats(marks):
     return max(marks),min(marks),sum(marks)/len(marks)
 
+#is_even
+def even(num):
+    return num%2==0
+
+#grade_calculate
+def grade(marks):
+    if marks>90:
+        return "A"
+    elif marks >= 75:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    elif marks >= 40:
+        return "D"
+    else:
+        return "F"
+
+
 #function call
 print(greet("Muskan"))
 
@@ -19,3 +37,9 @@ print(greet_student("Muskan"))
 marks=[98,56,78,50,99]
 high,low,avg=stats(marks)
 print(f"Highest marks :{high},lowest marks:{low},average marks :{avg}")
+
+num=89
+print(f"is number even :{even(num)}")
+
+print(grade(89))
+print(grade(99))
