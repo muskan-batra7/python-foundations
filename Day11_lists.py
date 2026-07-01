a26bfd643143c0bfecda34b89fa7787ff536c5d2
muskan <muskan20071676@gmail.com>
2026-07-01 23:28:40 +0530

    Day 11 - List and list methods

---
+++ Day11_lists.py
@@ -0,0 +1,8 @@
+#lists and list methods
+
+#basic operations
+mylist=[98,68,84,56,99,97]
+print(mylist[0])      #first item
+print(mylist[-1])     #last item
+print(mylist[1:3])    #[68, 84]
+print(mylist[-4:-1])  #[84, 56, 99]
